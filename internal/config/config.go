0fcc99e4cbbfacc02cbae5a34935b0af7cf7fb4b
Nickolsky <romb.intu@gmail.com>
2024-07-12 19:19:07 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"flag"
+	"os"
+	"strconv"
 )
 
 type ServerConfig struct {
@@ -28,6 +30,47 @@ func LoadServerConfigFromFlags() ServerConfig {
 	config.Address = *a
 	config.StorageDriver = *s
 	config.Environment = *e
+	return config
+}
+
+// Load Agent Config from Environment, if any var empty - load from flags or set default
+func LoadAgentConfig() AgentConfig {
+	var config AgentConfig
+	fromFlags := LoadAgentConfigFromFlags()
+
+	// Try load ADDRESS (ServerUrl)
+	address, ok := os.LookupEnv("ADDRESS")
+	if !ok {
+		config.ServerURL = fromFlags.ServerURL
+	} else {
+		config.ServerURL = address
+	}
+
+	// Try load REPORT_INTERVAL
+	ri, ok := os.LookupEnv("REPORT_INTERVAL")
+	if !ok {
+		config.ReportInterval = fromFlags.ReportInterval
+	} else {
+		parseRI, err := strconv.ParseInt(ri, 10, 64)
+		if err != nil {
+			config.ReportInterval = fromFlags.ReportInterval
+		} else {
+			config.ReportInterval = parseRI
+		}
+	}
+
+	// Try load POLL_INTERVAL
+	pi, ok := os.LookupEnv("POLL_INTERVAL")
+	if !ok {
+		config.PollInterval = fromFlags.PollInterval
+	} else {
+		parsePI, err := strconv.ParseInt(pi, 10, 64)
+		if err != nil {
+			config.PollInterval = fromFlags.PollInterval
+		} else {
+			config.PollInterval = parsePI
+		}
+	}
 	return config
 }
 
